a0d7212d94fb1ac3456fb42803792f71b1cb04a6
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2023-04-25 09:22:59 -0400

    Removed and Added Files
    Removed codeReview3.py temp file.
    Added Eicholtz starter code, title transfer.py

--- codeReview3.py
+++
@@ -1,4 +0,0 @@
-# codeReview3.py
-# Classification of Shirts, based on which CS professor they could belong to in the Florida Southern College CS department.
-# Classification is done using a deep neural network and transformation.
-
